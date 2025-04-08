5d3b7240807ba58b52317bc4b7580a2814540085
rlule15 <Rafael.lule10@yahoo.com>
2025-04-08 16:51:37 -0500

    Added Updated PHP scripts

---
+++ Web/media_tracker_web/src/php/authentication/create_user.php
@@ -0,0 +1,29 @@
+<?php
+    require '../db.php';
+
+    try{
+        if (isset($_POST['username']) &&
+        isset($_POST['first_name']) &&
+        isset($_POST['email']) &&
+        isset($_POST['password'])) {
+
+        $username = $_POST['username'];
+        $first = $_POST['first_name'];
+        $last = $_POST['last_name'] ?? null;
+        $email = $_POST['email'];
+        $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+        $stmt = $pdo->prepare("SELECT public.\"CreateUser\"(?, ?, ?, ?, ?)");
+        $stmt->execute([$username, $first, $last, $email, $password]);
+
+        echo "User created!";
+        } else {
+            echo "Required fields are missing.";
+        }
+    } catch (PDOException $e) {
+        // Handle errors by catching the exception
+        echo "Error: " . $e->getMessage();
+    }
+
+    $pdo = null;
+?>
