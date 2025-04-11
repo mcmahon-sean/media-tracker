2ae77c241795ed64c133168ba7fa7a6400a5dcdc
Crescend0h <jacobandmike2001@gmail.com>
2025-04-11 16:22:07 -0500

    Added Signup Form File
    Implemented a new form for the Sign-up function, Form controls are placed but not implemented.

---
+++ Desktop/media-tracker-desktop/media-tracker-desktop/SignupForm.Designer.cs
@@ -0,0 +1,307 @@
+﻿namespace media_tracker_desktop
+{
+    partial class SignupForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            panel1 = new Panel();
+            label5 = new Label();
+            label3 = new Label();
+            label2 = new Label();
+            label1 = new Label();
+            UNErr = new Label();
+            btnRegister = new Button();
+            btnCancel = new Button();
+            label4 = new Label();
+            txtConfirm = new TextBox();
+            PWLabel = new Label();
+            txtPassword = new TextBox();
+            LNameLabel = new Label();
+            txtLN = new TextBox();
+            FNameLabel = new Label();
+            txtFN = new TextBox();
+            UNLabel = new Label();
+            txtUsername = new TextBox();
+            panel2 = new Panel();
+            SignUpLabel = new Label();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.GrayText;
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label3);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(UNErr);
+            panel1.Controls.Add(btnRegister);
+            panel1.Controls.Add(btnCancel);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(txtConfirm);
+            panel1.Controls.Add(PWLabel);
+            panel1.Controls.Add(txtPassword);
+            panel1.Controls.Add(LNameLabel);
+            panel1.Controls.Add(txtLN);
+            panel1.Controls.Add(FNameLabel);
+            panel1.Controls.Add(txtFN);
+            panel1.Controls.Add(UNLabel);
+            panel1.Controls.Add(txtUsername);
+            panel1.Controls.Add(panel2);
+            panel1.Location = new Point(12, 12);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(776, 426);
+            panel1.TabIndex = 0;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.Red;
+            label5.Location = new Point(590, 324);
+            label5.Name = "label5";
+            label5.Size = new Size(55, 21);
+            label5.TabIndex = 18;
+            label5.Text = "xyxyxy";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Red;
+            label3.Location = new Point(590, 270);
+            label3.Name = "label3";
+            label3.Size = new Size(55, 21);
+            label3.TabIndex = 17;
+            label3.Text = "xyxyxy";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.Red;
+            label2.Location = new Point(590, 216);
+            label2.Name = "label2";
+            label2.Size = new Size(55, 21);
+            label2.TabIndex = 16;
+            label2.Text = "xyxyxy";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.Red;
+            label1.Location = new Point(590, 162);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 21);
+            label1.TabIndex = 15;
+            label1.Text = "xyxyxy";
+            // 
+            // UNErr
+            // 
+            UNErr.AutoSize = true;
+            UNErr.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            UNErr.ForeColor = Color.Red;
+            UNErr.Location = new Point(590, 103);
+            UNErr.Name = "UNErr";
+            UNErr.Size = new Size(55, 21);
+            UNErr.TabIndex = 14;
+            UNErr.Text = "xyxyxy";
+            // 
+            // btnRegister
+            // 
+            btnRegister.Font = new Font("Sans Serif Collection", 11.9999981F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnRegister.Location = new Point(386, 372);
+            btnRegister.Name = "btnRegister";
+            btnRegister.Size = new Size(198, 40);
+            btnRegister.TabIndex = 13;
+            btnRegister.Text = "Register";
+            btnRegister.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            btnCancel.Font = new Font("Sans Serif Collection", 11.9999981F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnCancel.ImageAlign = ContentAlignment.MiddleRight;
+            btnCancel.Location = new Point(278, 372);
+            btnCancel.Name = "btnCancel";
+            btnCancel.Size = new Size(98, 40);
+            btnCancel.TabIndex = 12;
+            btnCancel.Text = "Cancel";
+            btnCancel.TextAlign = ContentAlignment.TopCenter;
+            btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Sans Serif Collection", 11.9999981F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = SystemColors.HighlightText;
+            label4.Location = new Point(214, 315);
+            label4.Name = "label4";
+            label4.Size = new Size(162, 54);
+            label4.TabIndex = 11;
+            label4.Text = "Confirm Password";
+            // 
+            // txtConfirm
+            // 
+            txtConfirm.Location = new Point(386, 326);
+            txtConfirm.Name = "txtConfirm";
+            txtConfirm.Size = new Size(198, 23);
+            txtConfirm.TabIndex = 10;
+            // 
+            // PWLabel
+            // 
+            PWLabel.AutoSize = true;
+            PWLabel.Font = new Font("Sans Serif Collection", 11.9999981F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            PWLabel.ForeColor = SystemColors.HighlightText;
+            PWLabel.Location = new Point(278, 261);
+            PWLabel.Name = "PWLabel";
+            PWLabel.Size = new Size(98, 54);
+            PWLabel.TabIndex = 9;
+            PWLabel.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(386, 272);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(198, 23);
+            txtPassword.TabIndex = 8;
+            // 
+            // LNameLabel
+            // 
+            LNameLabel.AutoSize = true;
+            LNameLabel.Font = new Font("Sans Serif Collection", 11.9999981F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            LNameLabel.ForeColor = SystemColors.HighlightText;
+            LNameLabel.Location = new Point(278, 207);
+            LNameLabel.Name = "LNameLabel";
+            LNameLabel.Size = new Size(101, 54);
+            LNameLabel.TabIndex = 7;
+            LNameLabel.Text = "Last Name";
+            // 
+            // txtLN
+            // 
+            txtLN.Location = new Point(386, 218);
+            txtLN.Name = "txtLN";
+            txtLN.Size = new Size(198, 23);
+            txtLN.TabIndex = 6;
+            // 
+            // FNameLabel
+            // 
+            FNameLabel.AutoSize = true;
+            FNameLabel.Font = new Font("Sans Serif Collection", 11.9999981F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FNameLabel.ForeColor = SystemColors.HighlightText;
+            FNameLabel.Location = new Point(278, 153);
+            FNameLabel.Name = "FNameLabel";
+            FNameLabel.Size = new Size(103, 54);
+            FNameLabel.TabIndex = 5;
+            FNameLabel.Text = "First Name";
+            // 
+            // txtFN
+            // 
+            txtFN.Location = new Point(386, 164);
+            txtFN.Name = "txtFN";
+            txtFN.Size = new Size(198, 23);
+            txtFN.TabIndex = 4;
+            // 
+            // UNLabel
+            // 
+            UNLabel.AutoSize = true;
+            UNLabel.Font = new Font("Sans Serif Collection", 11.9999981F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            UNLabel.ForeColor = SystemColors.HighlightText;
+            UNLabel.Location = new Point(278, 94);
+            UNLabel.Name = "UNLabel";
+            UNLabel.Size = new Size(102, 54);
+            UNLabel.TabIndex = 3;
+            UNLabel.Text = "Username";
+            // 
+            // txtUsername
+            // 
+            txtUsername.Location = new Point(386, 105);
+            txtUsername.Name = "txtUsername";
+            txtUsername.Size = new Size(198, 23);
+            txtUsername.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ActiveCaptionText;
+            panel2.Controls.Add(SignUpLabel);
+            panel2.Location = new Point(3, 3);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(770, 88);
+            panel2.TabIndex = 1;
+            // 
+            // SignUpLabel
+            // 
+            SignUpLabel.AutoSize = true;
+            SignUpLabel.Font = new Font("Sans Serif Collection", 15.7499981F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            SignUpLabel.ForeColor = SystemColors.HighlightText;
+            SignUpLabel.Location = new Point(334, 16);
+            SignUpLabel.Name = "SignUpLabel";
+            SignUpLabel.Size = new Size(104, 72);
+            SignUpLabel.TabIndex = 0;
+            SignUpLabel.Text = "Sign Up";
+            SignUpLabel.TextAlign = ContentAlignment.MiddleCenter;
+            // 
+            // SignupForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(panel1);
+            Name = "SignupForm";
+            Text = "SignupForm";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private Label SignUpLabel;
+        private Label UNLabel;
+        private TextBox txtUsername;
+        private Label label4;
+        private TextBox txtConfirm;
+        private Label PWLabel;
+        private TextBox txtPassword;
+        private Label LNameLabel;
+        private TextBox txtLN;
+        private Label FNameLabel;
+        private TextBox txtFN;
+        private Button btnRegister;
+        private Button btnCancel;
+        private Label label5;
+        private Label label3;
+        private Label label2;
+        private Label label1;
+        private Label UNErr;
+    }
+}
---
+++ Desktop/media-tracker-desktop/media-tracker-desktop/SignupForm.cs
@@ -0,0 +1,86 @@
+﻿using media_tracker_desktop.Models;
+using media_tracker_desktop.Models.LastFM;
+using media_tracker_desktop.Models.Steam;
+using media_tracker_desktop.Models.SupabaseTables;
+using media_tracker_desktop.Models.TMDB;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using Supabase;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace media_tracker_desktop
+{
+    public partial class SignupForm : Form
+    {
+        public SignupForm()
+        {
+            InitializeComponent();
+        }
+
+        private Client connection;
+
+        //Button code copied from Form1.cs
+        private async void btnTestUserCreate_Click(object sender, EventArgs e)
+        {
+            if (connection == null)
+            {
+                MessageBox.Show("Not connected to the DB.");
+                return;
+            }
+
+            UserAppAccount account = new UserAppAccount(connection);
+
+            // variable was used to control a while loop, which loops again if the user didn't enter valid fields to create a new user.
+            bool validUser = false;
+
+
+            // Default Username = "testDesktopUser"
+            // Default Password = "testDesktopPassword"
+            string username = txtUsername.Text;
+            string firstName = "testDesktopFN";
+            string lastName = "testDesktopLN";
+            string email = "testDesktop@email.com";
+            string password = txtPassword.Text;
+
+            UserRegistrationParam newUser = null;
+
+            // try catch block is implemented since the SupabaseUserRegistration object throws exception as a means of validation.
+            try
+            {
+                newUser = new UserRegistrationParam(username, firstName, lastName, email, password);
+
+                // If the user object is not null,
+                if (newUser != null)
+                {
+                    // Create the user.
+                    (bool userCreated, string message) result = await account.CreateUser(newUser);
+
+                    // If the creation is successful,
+                    if (result.userCreated)
+                    {
+                        MessageBox.Show($"User: {newUser.Username} created.");
+
+                        validUser = true;
+                    }
+                    else
+                    {
+                        MessageBox.Show(result.message);
+
+                        validUser = false;
+                    }
+                }
+                else
+                {
+                    validUser = false;
+                }
+            }
+            catch (Exception error)
+            {
+                validUser = false;
+            }
+        }
+
+    }
+}
