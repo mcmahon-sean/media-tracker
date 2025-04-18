97797c4fdf7a41c036e8014486825b4e90461b5f
IzzetCoder <136272211+IzzetCoder@users.noreply.github.com>
2025-04-17 22:44:11 -0500

    Added media data models (by: Ahmed)
    -Added media data models for each media api.
    -All credit goes to Ahmed Dahir for creating the models.

---
+++ Web/Media_Tracker_Web/models/Last.FM/LastFmTrack.php
@@ -0,0 +1,35 @@
+<?php
+
+class LastFmTrack {
+    public string $name;
+    public string $artist;
+    public string $album;
+    public ?string $playCount;
+    public string $url;
+    public ?int $timestamp;
+    public ?string $mbid;
+    public ?string $rank;
+    public ?string $imageUrl;
+
+    public function __construct(array $json) {
+        $imageList = array_filter($json['image'] ?? [], fn($img) => !empty($img['#text']));
+        $image = end($imageList);
+        $image = $image ?: ['#text' => null];
+    
+        $this->name = $json['name'];
+        $this->artist = $json['artist']['#text'] ?? '';
+        $this->album = $json['album']['#text'] ?? '';
+        $this->playCount = intval($json['playcount'] ?? 0);
+        $this->url = $json['url'];
+        $this->timestamp = isset($json['date']['uts']) ? intval($json['date']['uts']) : null;
+        $this->mbid = $json['mbid'] ?? null;
+        $this->rank = intval($json['@attr']['rank'] ?? 0);
+        $this->imageUrl = $image['#text'];
+    }
+    
+
+    public function getFormattedDate(): ?string {
+        if (!$this->timestamp) return null;
+        return date('Y-m-d H:i', $this->timestamp);
+    }
+}
