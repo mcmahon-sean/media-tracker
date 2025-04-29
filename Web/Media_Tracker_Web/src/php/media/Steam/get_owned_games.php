2eeb0a330e287b4a8f91cf3e9ec668fd8102eb65
Rafael Lule <Rafael.lule10@yahoo.com>
2025-04-29 18:01:50 -0500

    Fixed error handling for steam errors

--- Web/Media_Tracker_Web/src/php/media/Steam/get_owned_games.php
+++ Web/Media_Tracker_Web/src/php/media/Steam/get_owned_games.php
@@ -3,31 +3,34 @@
 require_once '../config.php';
 require_once '../models/Steam/SteamGame.php';
 
-$steam_id = $_SESSION['user_platform_ids']['steam'];
+$steam_id = isset($_SESSION['user_platform_ids']['steam']);
+$error = null;
 
 if (!$steam_id) {
-    die("Steam ID is missing.");
-}
+    $error = "Steam ID is missing.";
+}else{
+    // Build the Steam API URL
+    $apiUrl = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?" . http_build_query([
+        'key' => STEAM_API_KEY,
+        'steamid' => $steam_id,
+        'include_appinfo' => true,
+        'include_played_free_games' => true
+    ]);
 
-// Build the Steam API URL
-$apiUrl = "https://api.steampowered.com/IPlayerService/GetOwnedGames/v1/?" . http_build_query([
-    'key' => STEAM_API_KEY,
-    'steamid' => $steam_id,
-    'include_appinfo' => true,
-    'include_played_free_games' => true
-]);
+    // Make the API call
+    $response = file_get_contents($apiUrl);
+    $data = json_decode($response, true);
 
-// Make the API call
-$response = file_get_contents($apiUrl);
-$data = json_decode($response, true);
+    // Array to hold SteamGame objects
+    $steamGames = [];
 
-// Array to hold SteamGame objects
-$steamGames = [];
-
-if (isset($data['response']['games'])) {
-    foreach ($data['response']['games'] as $gameData) {
-        $ownedGames[] = new SteamGame($gameData);
+    if (isset($data['response']['games'])) {
+        foreach ($data['response']['games'] as $gameData) {
+            $ownedGames[] = new SteamGame($gameData);
+        }
+    } else {
+        $error = "No games found. This Steam profile might be private.";
     }
-} else {
-    echo "No games found. This Steam profile might be private.";
 }
+
+
