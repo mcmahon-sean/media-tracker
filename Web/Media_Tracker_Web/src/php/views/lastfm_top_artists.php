0977c37624eb062441a3b4d70458dce68ce6f89a
AlexisB-SCTCC <ls5754vw@go.minnstate.edu>
2025-04-30 11:24:16 -0500

    Form & Header Style Adjustments

--- Web/Media_Tracker_Web/src/php/views/lastfm_top_artists.php
+++ Web/Media_Tracker_Web/src/php/views/lastfm_top_artists.php
@@ -71,7 +71,12 @@
 
             <main class="col-md-10 ms-sm-auto px-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                    <h2>Media Tracker</h2>
+                    <h2 class="display-6">
+                        Media Tracker
+                        <small class="text-title-secondary">
+                            Top Artists
+                        </small>
+                    </h2>
                 </div>
 
                 <div class="card bg-dark text-light mb-4">
